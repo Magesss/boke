5760ff754f9548f08cc4a1a63ad52bd673aff308
Magesss <15234447741@163.com>
2020-07-31 09:38:16 +0800

    前端路由配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,39 @@
-import React, {Component} from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import routes from './router'
+import './App.css'
+import {
+  Route,
+  Redirect,
+  Switch
+} from 'react-router-dom'
 
-class App extends React.Component {
-  componentDidMount() {
-    fetch('/getUserInfo',{
-      method:'get',
-    }).then(data=>{
-      console.log(data)
-    })
-  }
-  render(){
-    return (
+
+class App extends Component {
+  render() {
+    return(
       <div className="App">
-        <button>ces</button>
+        <header>头部</header>
+        <Switch>
+          {
+            routes.map(route => {
+              return(
+                <Route
+                  key={route.path}
+                  path={route.path}
+                  component={route.component}
+                />
+              )
+            })
+          }
+          <Redirect exact from="/" to={routes[0].path}></Redirect>
+          {/* 这里用 redirect 进行 首页自动跳转到 /home 路由下
+              exact 意味着精确匹配 当为 / 时才跳转 /home 不是包含 / 就跳转到 /home
+          */}
+          <Redirect to='/404/'></Redirect>
+        </Switch>
+        <footer>底部</footer>
       </div>
-    );
+    )
   }
 }
 
---
+++ src/pages/login/index.tsx
@@ -0,0 +1,12 @@
+import React, { FC, useEffect } from 'react';
+import { Button } from 'antd';
+
+const Login: FC = () => {
+  useEffect(() => {
+  })
+  return (
+    <Button type="primary" size="large">login</Button>
+  )
+};
+
+export default Login;
