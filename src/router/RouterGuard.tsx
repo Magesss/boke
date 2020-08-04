fa3f1d5a80e3f57b0f7a221f6e2ce5db4de18696
Magesss <15234447741@163.com>
2020-08-04 10:41:42 +0800

    登录后屏蔽登录、注册等页面

--- src/router/RouterGuard.tsx
+++ src/router/RouterGuard.tsx
@@ -15,13 +15,18 @@ class RouterGuard extends Component{
   componentDidMount() {
     // @ts-ignore
     let {history: {replace}, location} = this.props
+    let whiteList:Array<string> = ['/login']
     if (!sessionStorage.getItem('access_token')) replace('/login')
     if (location.pathname === '/') replace('./home')
+    // 屏蔽 登录后不能去的页面
+    const i:number = whiteList.findIndex((v) => {
+      return location.pathname.includes(v)
+    })
+    if(i > -1) replace('/home')
   }
   render() {
     // @ts-ignore
     let { component, routes = [] } = this.props
-    console.log('routes: ', routes)
     const LoadableComponent = component
     return (
       <Fragment>
