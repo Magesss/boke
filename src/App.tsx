c26c2737aa336aee128bd1f0b2b14e54fecc9c74
Magesss <15234447741@163.com>
2020-08-04 10:26:54 +0800

    重定向，404

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
 import './App.css'
 import RouterComponent from './router/index'
+import {
+  Switch,
+  Redirect
+} from 'react-router-dom'
 
 class App extends Component {
   render() {
     return(
-      <RouterComponent />
+      <Switch>
+        <RouterComponent />
+        <Redirect exact from="/" to='/home'></Redirect>
+      </Switch>
     )
   }
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,23 +1,35 @@
 import React, { Component } from 'react';
 import {
   Route,
+  Redirect,
   Switch
 } from 'react-router-dom'
 import RouterGuard from "./RouterGuard";
 import routerConfig from "./routerConfig";
 
 
+
 const renderRoutesMap = (routes:any) => {
+  let redc1:any
+  let redc2:any
   if (!Array.isArray(routes)) {
     routes = routerConfig
+    redc1 = <Redirect exact from="/" to='/home'></Redirect>
+    redc2 = <Redirect to='/404'></Redirect>
   }
   return (
-    routes.map((route:any, index:any) => {
-      return (
-        <Route key={index} path={route.path} render={props => <RouterGuard {...route} {...props} />}
-        />
-      )
-    })
+    <Switch>
+      {
+        routes.map((route:any, index:any) => {
+          return (
+            <Route key={index} path={route.path} render={props => <RouterGuard {...route} {...props} />}
+            />
+          )
+        })
+      }
+      {redc1}
+      {redc2}
+    </Switch>
   )
 }
 
